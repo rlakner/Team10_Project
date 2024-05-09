206ec1fc276768984b163a9c3d4a23a0ef2041a4
Roman Lakner <rlakner@umd.edu>
2024-05-09 13:36:55 -0400

    Separated scrape_items and removed scrape_images

--- final.py
+++ final.py
@@ -8,6 +8,9 @@ from tkinter import scrolledtext
 import pandas as pd
 from data_analysis import Analysis
 
+service = Service(executable_path="../chromedriver.exe") #Change to your path
+driver = webdriver.Chrome(service=service)
+
 class Item:
     """A class for data representing an item listed on Depop
     
@@ -33,6 +36,7 @@ class Item:
         self.price = price
         self.condition = condition
         self.link = link
+        
 class Search:
     """A class for scraping and analyzing data for items listed on Depop"""
     def __init__(self, query):
@@ -46,22 +50,17 @@ class Search:
         
         self.query = query
         self.items_list = []
-        
-    def scrape_items(self):
+    
+    def init_scrape(self):
         """Uses Selenium and BeautifulSoup to navigate through Depop listings
-        and scrape information regarding the name, price, condition, and link 
-        of the items
+        and scrape information regarding the link and price
 
         Args: None
 
-        Returns: 
-            items_list(list): A list of Item objects that contain the name, 
-            price, condition, and link of the scraped item
+        Returns: None
         """
         
         url = f'https://www.depop.com/search/?q={self.query}'
-        service = Service(executable_path="/usr/local/bin/chromedriver") #Change to your path
-        driver = webdriver.Chrome(service=service)
         driver.get(url)
 
         x = 0
@@ -75,8 +74,9 @@ class Search:
         soup = bs(source, "html.parser")
         listings = soup.find('ul', class_='styles__ProductListGrid-sc-4aad5806-1 hGGFgp')
 
-        temp_list = []
-
+        self.temp_list = []
+        self.check = 0
+        
         for listing in listings:
             link = f"http://depop.com{listing.find('a', class_='styles__ProductCard-sc-4aad5806-4 ffvUlI')['href']}"
             # gets the price
@@ -86,62 +86,52 @@ class Search:
             else:
                 price_element = listing.find('p', class_='sc-eDnWTT Price-styles__FullPrice-sc-f7c1dfcc-0 fRxqiS hmFDou')
                 price = float(price_element.text.strip('$'))
-            temp_list.append([price, link])   
+            self.temp_list.append([price, link])
+              
+        self.check = 1
         
-        for item in temp_list:
-            driver.get(item[1])
-            item_source = driver.page_source
-            item_soup = bs(item_source, "html.parser")  
-            name_div = item_soup.find('div', class_='styles__ContentWrapper-sc-569ef83f-3 cyzIGA')
-            find_name = name_div.find('h1', class_='sc-grYavY styles__MobileProductTitle-sc-569ef83f-9 HXICV cTNEru')
-            brand_div = item_soup.find('div', class_ = 'ProductAttributes-styles__Attributes-sc-303d66c3-1 dIfGXO styles__StyledProductAttributes-sc-569ef83f-16 hcLsNE')
-            find_brand = brand_div.find('a', class_='sc-eDnWTT ProductAttributes-styles__Attribute-sc-303d66c3-0 kcKICQ iIJjeL')
-            find_condition = item_soup.find_all('p', class_='sc-eDnWTT ProductAttributes-styles__Attribute-sc-303d66c3-0 kcKICQ iIJjeL')[1]
-            if find_name:
-                name = find_name.text
-            else:
-                brand = find_brand.text
-                name = f"{brand} Item"
-                
-            if find_condition:
-                condition_text = find_condition.text.lower()
-                if "new" in condition_text:
-                    condition = "New"
-                else:
-                    condition = "Used"
-            self.items_list.append(Item(name, item[0], condition, item[1]))
-        
-        time.sleep(10)
-        driver.quit()
-        return self.items_list
-    
-    def scrape_images(self):
-        """Uses BeautifulSoup to scrape the images of the items associated with 
-            the gathered unique direct links from scrape_items.
+    def scrape_items(self):
+        """Uses Selenium and BeautifulSoup to navigate through Depop listings
+        and scrape information on each individual page regarding the name and
+        condition
 
-        Args:
-            items(list): The list of Items returned by scrape_items which 
-                will then be parsed to extract the unique direct links
+        Args: None
 
         Returns: 
-            images(list): A list of dictionaries of the item images to be 
-            displayed in the user interface, having the direct links as keys and 
-            the image source links as values
+            items_list(list): A list of Item objects that contain the name, 
+            price, condition, and link of the scraped item
         """
-        images = []
-        service = Service(executable_path="/usr/local/bin/chromedriver") #Change to your path
-        driver = webdriver.Chrome(service=service)
         
-        for item in self.items_list:
-            driver.get(item.link)
-            item_source = driver.page_source
-            item_soup = bs(item_source, "html.parser")
-            info_div = item_soup.find('div', class_='styles__Layout-sc-569ef83f-2 MYFCM')
-            img = info_div.find('img')['src']
-            name = info_div.find('img')['alt']
+        self.init_scrape()
+        
+        while self.check == 1:
+        
+            for item in self.temp_list:
+                driver.get(item[1])
+                item_source = driver.page_source
+                item_soup = bs(item_source, "html.parser")  
+                name_div = item_soup.find('div', class_='styles__ContentWrapper-sc-569ef83f-3 cyzIGA')
+                find_name = name_div.find('h1', class_='sc-grYavY styles__MobileProductTitle-sc-569ef83f-9 HXICV cTNEru')
+                brand_div = item_soup.find('div', class_ = 'ProductAttributes-styles__Attributes-sc-303d66c3-1 dIfGXO styles__StyledProductAttributes-sc-569ef83f-16 hcLsNE')
+                find_brand = brand_div.find('a', class_='sc-eDnWTT ProductAttributes-styles__Attribute-sc-303d66c3-0 kcKICQ iIJjeL')
+                find_condition = item_soup.find_all('p', class_='sc-eDnWTT ProductAttributes-styles__Attribute-sc-303d66c3-0 kcKICQ iIJjeL')[1]
+                if find_name:
+                    name = find_name.text
+                else:
+                    brand = find_brand.text
+                    name = f"{brand} Item"
+                    
+                if find_condition:
+                    condition_text = find_condition.text.lower()
+                    if "new" in condition_text:
+                        condition = "New"
+                    else:
+                        condition = "Used"
+                self.items_list.append(Item(name, item[0], condition, item[1]))
             
-            images.append({img: name})
-        return images
+            time.sleep(10)
+            driver.quit()
+            return self.items_list
     
 class Interface:
     """
