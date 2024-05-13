499184cb1aa12030213184c409c7d406f535046e
Roman Lakner <rlakner@umd.edu>
2024-05-13 19:29:33 -0400

    Fixed

--- deal_finder.py
+++ deal_finder.py
@@ -9,7 +9,7 @@ import pandas as pd
 from data_analysis import Analysis
 
 service = Service(executable_path=
-                          "../chromedriver.exe") #Change to your path
+                          "/usr/local/bin/chromedriver") #Change to your path
 driver = webdriver.Chrome(service=service)
 
 class Item:
